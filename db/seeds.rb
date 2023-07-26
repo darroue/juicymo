e75f891a23c728703d6c63f6a2cc4ecbe69b2efb
Petr Radouš <darroue@gmail.com>
2023-07-26 13:42:41 +0200

    Finalized seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -15,22 +16,39 @@ include FactoryBot::Syntax::Methods
 user_emails = []
 
 3.times do
-  user = create(:user, password: "123456")
+  user = begin
+    create(:user, password: '123456')
+  rescue StandardError
+    next
+  end
   user_emails << user.email
 
+  projects = []
   30.times do
-    create(:project, user: user)
+    projects << attributes_for(:project, user_id: user.id)
+  rescue StandardError
+    next
   end
+  projects = Project.create(projects)
 
+  tags = []
   30.times do
-    create(:tag, user: user)
+    tags << attributes_for(:tag, user_id: user.id)
+  rescue StandardError
+    next
   end
+  tags = Tag.create(tags)
 
-  Project.for_user(user).each do |project|
+  tasks = []
+  projects.each do |project|
     rand(30).times do
-      create(:task, project: project)
+      tasks << attributes_for(:task, project_id: project.id, tag_ids: tags.sample(rand(10)).map(&:id))
+    rescue StandardError
+      next
     end
   end
+
+  Task.create(tasks)
 end
 
 puts user_emails.inspect
